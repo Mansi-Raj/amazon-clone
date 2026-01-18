9b541f5189a419ecf93dc0750eb592c742d307c3
MANSI RAJ <157103259+Mansi-Raj@users.noreply.github.com>
2026-01-18 19:07:37 +0530

    home page creation

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -3,9 +3,9 @@ import { Product } from './Product';
 
 export function HomePage(){
   return(
-    <>
+    <div className="homePage">
       <Header />
       <Product />
-    </>
+    </div>
   );
 }
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,5 +1,6 @@
 import { products } from '../data/products';
 import { moneyFormatting } from '../utilities/moneyFormatting';
+import './product.css';
 
 export function Product(){
   return(
@@ -7,19 +8,19 @@ export function Product(){
       <div className="products-grid js-products-grid">
         {products.map((product) => {
           return(
-            <div className="product-container">
+            <div key={product.id} className="product-container">
               <div className="product-image-container">
-                <img className="product-image" src="${product.image}" />
+                <img className="product-image" src={product.image} />
               </div>
             
               <div className="product-name limit-text-to-2-lines"> 
-                ${product.name}
+                {product.name}
               </div>
             
               <div className="product-rating-container">
-                <img src="images/ratings/rating-${product.rating.stars * 10}.png" alt="" className="product-rating-stars" />
+                <img src={`images/ratings/rating-${product.rating.stars * 10}.png`} alt="" className="product-rating-stars" />
                 <div className="product-rating-count link-primary">
-                  ${product.rating.count}
+                  {product.rating.count}
                 </div>
               </div>
             
@@ -28,18 +29,27 @@ export function Product(){
               </div>
             
               <div className="product-quantity-container">
-                <select className="js-quantity-selector-${product.id}">
-                  ${generateOptions(10)}
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
                 </select>
               </div>
             
               <div className="product-spacer"></div>
             
-              <div className="add-to-cart js-added-${product.id}">
+              <div className={`add-to-cart js-added-${product.id}`}>
               </div>
             
-              <button className="add-to-cart-button button-primary js-add-to-cart" data-product-id ="${product.id}">
-                    Add to Cart
+              <button className="add-to-cart-button button-primary js-add-to-cart" data-product-id ={product.id}>
+                Add to Cart
               </button>
             </div>
           );        
@@ -47,12 +57,4 @@ export function Product(){
       </div>
     </div>
   )
-}
-
-function generateOptions(count) {
-  let options = '';
-  for (let i = 1; i <= count; i++) {
-    options += `<option value="${i}">${i}</option>`;
-  }
-  return options;
 }
