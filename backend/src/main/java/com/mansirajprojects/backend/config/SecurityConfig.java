7df589a894e5d8f7efd69f56fd4ba0fe571ba923
MANSI RAJ <157103259+Mansi-Raj@users.noreply.github.com>
2026-02-26 20:40:25 +0530

    adminpage bug fixing

--- backend/src/main/java/com/mansirajprojects/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/mansirajprojects/backend/config/SecurityConfig.java
@@ -7,7 +7,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain; // Add this import
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.mansirajprojects.backend.security.JwtAuthFilter;
@@ -18,6 +20,11 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
